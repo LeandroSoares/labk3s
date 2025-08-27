89591b3a7147df8f6401abe1781a28797db5a2ab
Lssoares <leandrogamedesigner@gmail.com>
2025-08-27 02:22:57 -0300

    rebuild

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,21 +1,21 @@
-# Outputs do Terraform
+# # Outputs do Terraform
 
-output "grafana_url" {
-  description = "URL para acessar o Grafana"
-  value       = module.observability.grafana_url
-}
+# output "grafana_url" {
+#   description = "URL para acessar o Grafana"
+#   value       = module.observability.grafana_url
+# }
 
-output "prometheus_url" {
-  description = "URL para acessar o Prometheus"
-  value       = module.observability.prometheus_url
-}
+# output "prometheus_url" {
+#   description = "URL para acessar o Prometheus"
+#   value       = module.observability.prometheus_url
+# }
 
-output "namespace" {
-  description = "Namespace onde os serviços foram instalados"
-  value       = module.observability.namespace
-}
+# output "namespace" {
+#   description = "Namespace onde os serviços foram instalados"
+#   value       = module.observability.namespace
+# }
 
-output "cert_manager_instructions" {
-  description = "Instruções para configurar o ClusterIssuer"
-  value       = module.cert_manager.cluster_issuer_instructions
-}
+# output "cert_manager_instructions" {
+#   description = "Instruções para configurar o ClusterIssuer"
+#   value       = module.cert_manager.cluster_issuer_instructions
+# }
